0c2639bc4bbf2f182ed93d264120419c1b906f87
Javad Zobeidi <j.zobeidi89@gmail.com>
2024-01-15 22:42:34 +0330

    fixed left room bug

--- lib/src/websocket/websocket_session.dart
+++ lib/src/websocket/websocket_session.dart
@@ -40,44 +40,40 @@ class WebsocketSession {
     return _activeSessions[sessionId];
   }
 
- /// remove session of connected socket
+  /// remove session of connected socket
   void removeSession(String sessionId) {
     SessionInfo? info = _activeSessions[sessionId];
-    if(info != null){
-       _activeSessions.remove(sessionId);
-       leftRoom(sessionId, info.activeRoom);
-       info.websocket.close();
+    if (info != null) {
+      leftRoom(sessionId, info.activeRoom);
+      _activeSessions.remove(sessionId);
+      info.websocket.close();
     }
   }
 
- 
   void joinRoom(String sessionId, String roomId) {
-    
-    if(_rooms[roomId] == null){
+    if (_rooms[roomId] == null) {
       _rooms[roomId] = <String>[];
     }
     _rooms[roomId]?.add(sessionId);
     SessionInfo? info = _activeSessions[sessionId];
-    if(info != null){
-      if(info.previousRoom != null){
-       leftRoom(sessionId, info.previousRoom);
+    if (info != null) {
+      if (info.previousRoom != null) {
+        leftRoom(sessionId, info.previousRoom);
       }
       info.activeRoom = roomId;
       info.activeRoom = roomId;
     }
 
-    print(_rooms);
-    
   }
 
   void leftRoom(String sessionId, String? roomId) {
-    if(_rooms[roomId] == null && roomId != null){
+    if (_rooms[roomId] != null && roomId != null) {
       SessionInfo? info = _activeSessions[sessionId];
-    if(info != null){
-      info.previousRoom = null;
-      info.activeRoom = null;
-      _rooms[roomId]?.remove(sessionId);
-    }
+      if (info != null) {
+        info.previousRoom = null;
+        info.activeRoom = null;
+        _rooms[roomId]?.remove(sessionId);
+      }
     }
   }
 
@@ -87,16 +83,15 @@ class WebsocketSession {
     return _rooms[roomId] ?? <String>[];
   }
 
-  bool isRoom(String roomId){
+  bool isRoom(String roomId) {
     return _rooms[roomId] != null ? true : false;
   }
 
-  bool isSession(String sessionId){
+  bool isSession(String sessionId) {
     return _activeSessions[sessionId] != null ? true : false;
   }
 
-  List<SessionInfo> getActiveSessions(){
+  List<SessionInfo> getActiveSessions() {
     return _activeSessions.values.toList();
   }
-
 }
