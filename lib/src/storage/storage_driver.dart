5401dc36f1388ae73565585467ea5bdc5272ab9e
Javad Zobeidi <j.zobeidi89@gmail.com>
2024-03-17 14:49:35 +0330

    Adding Full path to the storage

--- lib/src/storage/storage_driver.dart
+++ lib/src/storage/storage_driver.dart
@@ -2,13 +2,15 @@ import 'dart:typed_data';
 
 abstract class StorageDriver {
   Future<String> put(
-    String filePath,
+    String fileName,
     List<int> bytes,
   );
 
-  Future<Uint8List?> get(String filepath);
+  Future<Uint8List?> get(String fileName);
+  
+  String fullPath(String file);
 
-  Future<bool> exists(String filepath);
+  Future<bool> exists(String fileName);
 
-  Future<dynamic> delete(String filepath);
+  Future<dynamic> delete(String fileName);
 }
