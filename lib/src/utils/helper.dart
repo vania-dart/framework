852e5d536c3c47cb91382aa21454aa1eff8e1805
Javad Zobeidi <j.zobeidi89@gmail.com>
2024-04-23 17:43:54 +0330

    Added connection to the helper file

--- lib/src/utils/helper.dart
+++ lib/src/utils/helper.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:eloquent/eloquent.dart';
 import 'package:vania/vania.dart';
 
 String storagePath(String file) => '${Directory.current.path}/storage/$file';
@@ -11,3 +12,5 @@ T env<T>(String key, [dynamic defaultValue]) => Env.get<T>(key, defaultValue);
 abort(int code, String message) {
   throw HttpResponseException(message: message, code: code);
 }
+
+Connection get connection => Config().get('database')?.driver?.connection;
