d9547593dde353015910d68ae5e7b1ad76d8a91d
Reza Taghizadeh <alirezataghizadeh66@gmail.com>
2024-06-02 16:44:41 +0200

    add otp to signup

--- example/todo_list/lib/app/http/controllers/auth/auth_controller.dart
+++ example/todo_list/lib/app/http/controllers/auth/auth_controller.dart
@@ -3,7 +3,6 @@ import 'dart:math';
 import 'package:todo_list/app/models/user.dart';
 import 'package:vania/vania.dart';
 
-
 class AuthController extends Controller {
   /// Login
   Future<Response> login(Request request) async {
@@ -61,36 +60,69 @@ class AuthController extends Controller {
         .where('email', '=', request.input('email'))
         .first();
     if (user != null) {
-      return Response.json({'message': 'User already exists'});
+      if (user['status'] != 'inactive') {
+        return Response.json({'message': 'User already exists'});
+      } else {
+        return otp(request);
+      }
     }
-
-    await User().query().insert({
+    Map<String, dynamic> newUser = {
       'first_name': request.input('first_name'),
       'last_name': request.input('last_name'),
       'email': request.input('email'),
       'password': Hash().make(request.input('password').toString()),
       'created_at': DateTime.now(),
       'updated_at': DateTime.now(),
-    });
+      'status': 'inactive',
+    };
+    await User().query().insert(newUser);
 
-    return Response.json({'message': 'User created successfully'});
+    return otp(request);
   }
 
   Future<Response> otp(Request request) {
+    request.validate({
+      'email': 'required|email',
+    }, {
+      'email.required': 'The email is required',
+      'email.email': 'The email is not valid',
+    });
     Random rnd = Random();
     int otp = rnd.nextInt(999999 - 111111);
-
-    Cache.put('otp', otp.toString(), duration: Duration(minutes: 3));
-
-    return Response.json({'message': 'OTP sent successfully'});
+    Cache.put(request.input('email'), otp.toString(),
+        duration: Duration(minutes: 3));
+    // ???: you can add here your code for sending otp using email or sms
+    return Response.json({
+      'message': 'OTP sent successfully',
+      'data': {'code': otp}
+    });
   }
 
   Future<Response> verifyOTO(Request request) async {
-    final String otp = request.input('otp');
-    final String otpValue = Cache.get('otp') as String;
+    request.validate({
+      'email': 'required|email',
+      'otp': 'required',
+    }, {
+      'email.required': 'The email is required',
+      'email.email': 'The email is not valid',
+      'otp.required': 'The otp is required',
+    });
+    Map<String, dynamic>? user = await User()
+        .query()
+        .where('email', '=', request.input('email'))
+        .first();
+    if (user == null) {
+      return Response.json({'message': 'User not found'});
+    }
+    final String otp = request.input(request.input('otp'));
+    final String otpValue = Cache.get(request.input('email')) as String;
 
     if (otpValue == otp) {
       Cache.delete('otp');
+
+      await User().query().where('email', request.input('email')).update({
+        'status': 'active',
+      });
       return Response.json({'message': 'OTP verified successfully'});
     } else {
       return Response.json(
