617d0537be5b90efffd01f7971de2a6e9c4f5b00
Javad Zobeidi <j.zobeidi89@gmail.com>
2024-06-06 13:27:04 +0330

    Add random int Refactor random string

--- lib/src/utils/functions.dart
+++ lib/src/utils/functions.dart
@@ -5,10 +5,26 @@ String sanitizeRoutePath(String path) {
   return path.replaceAll(RegExp('^\\/+|\\/+\$'), '');
 }
 
-String genrateRandomString([int length = 32]) {
-  const chars =
-      'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890';
+String randomString([int length = 32]) {
+  List<String> strList =
+      'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz'.split('');
+  strList.shuffle();
+  String chars = strList.join('');
   Random rnd = Random();
   return String.fromCharCodes(Iterable.generate(
       length, (_) => chars.codeUnitAt(rnd.nextInt(chars.length))));
+}
+
+T randomInt<T>([int length = 6]) {
+  List<String> strList = '1234567890'.split('');
+  strList.shuffle();
+  String chars = strList.join('');
+  Random rnd = Random();
+  String random = String.fromCharCodes(Iterable.generate(
+      length, (_) => chars.codeUnitAt(rnd.nextInt(chars.length))));
+
+  if (T is int) {
+    return int.parse(random) as T;
+  }
+  return random as T;
 }
