5ea9e16e2ad2387d805ca77797603dc9eadd6cf3
Javad Zobeidi <j.zobeidi89@gmail.com>
2024-06-06 13:27:37 +0330

    Add url helper Add assets helper

--- lib/src/utils/helper.dart
+++ lib/src/utils/helper.dart
@@ -6,6 +6,10 @@ String storagePath(String file) => 'storage/$file';
 
 String publicPath(String file) => 'public/$file';
 
+String url(String path) => '${env<String>('APP_URL')}/$path';
+
+String assets(String src) => url(src);
+
 T env<T>(String key, [dynamic defaultValue]) => Env.get<T>(key, defaultValue);
 
 abort(int code, String message) {
