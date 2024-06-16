5faec151574e7df66ff337ee464bb65a7bb196a8
Javad Zobeidi <j.zobeidi89@gmail.com>
2024-06-16 12:30:17 +0330

    Add DateTime format

--- lib/src/extensions/date_time_aws_format.dart
+++ lib/src/extensions/date_time_aws_format.dart
@@ -6,3 +6,14 @@ extension DateTimeAwsFormat on DateTime {
         '${zeroPad(hour)}${zeroPad(minute)}${zeroPad(second)}Z';
   }
 }
+
+extension DateTimeFormatting on DateTime {
+  String format() {
+    return '${year.toString().padLeft(4, '0')}-'
+        '${month.toString().padLeft(2, '0')}-'
+        '${day.toString().padLeft(2, '0')} '
+        '${hour.toString().padLeft(2, '0')}:'
+        '${minute.toString().padLeft(2, '0')}:'
+        '${second.toString().padLeft(2, '0')}';
+  }
+}
