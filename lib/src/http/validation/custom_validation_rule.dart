06f63e47ba7d4a870c6800dd4dcdc8381744e0a2
Javad Zobeidi <j.zobeidi89@gmail.com>
2025-02-05 10:17:00 +0330

    Fixed Custom validation rule Future bug

--- lib/src/http/validation/custom_validation_rule.dart
+++ lib/src/http/validation/custom_validation_rule.dart
@@ -1,7 +1,7 @@
 class CustomValidationRule {
   final String ruleName;
   final String message;
-  final bool Function(Map<String, dynamic>, dynamic, String?) fn;
+  final Future<bool> Function(Map<String, dynamic>, dynamic, String?) fn;
 
   CustomValidationRule({
     required this.ruleName,
