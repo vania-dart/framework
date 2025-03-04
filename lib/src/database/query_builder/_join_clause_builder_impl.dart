bb6b785441322f84a41b17970e30e8999e4068a7
Javad Zobeidi <j.zobeidi89@gmail.com>
2025-03-05 01:06:41 +0330

    add `JoinClauseBuilder` class for constructing `JOIN` clauses

---
+++ lib/src/database/query_builder/_join_clause_builder_impl.dart
@@ -0,0 +1,100 @@
+import '../../contract/database/query_builder/query_builder.dart'
+    show QueryBuilder;
+
+abstract mixin class JoinClauseBuilderImpl implements QueryBuilder {
+  @override
+  QueryBuilder crossJoin(
+    String table, [
+    List<dynamic> bindings = const [],
+  ]) {
+    String clause = "CROSS JOIN $table";
+    joins.add(clause);
+    return this;
+  }
+
+  @override
+  QueryBuilder join(
+    String table,
+    String firstColumn, [
+    String? operator,
+    String? secondColumn,
+    String type = 'inner',
+    bool where = false,
+  ]) {
+    String clause = "$type JOIN $table";
+    if (operator != null && secondColumn != null) {
+      clause += " ON $firstColumn $operator $secondColumn";
+    }
+    joins.add(clause);
+    return this;
+  }
+
+  @override
+  QueryBuilder joinSub(
+    QueryBuilder subQuery,
+    String as,
+    String firstColumn, [
+    String? operator,
+    String? secondColumn,
+    String type = 'inner',
+  ]) {
+    String subSql = "(${subQuery.toSql()}) AS $as";
+    String clause = "$type JOIN $subSql";
+    if (operator != null && secondColumn != null) {
+      clause += " ON $firstColumn $operator $secondColumn";
+    }
+    joins.add(clause);
+    return this;
+  }
+
+  @override
+  QueryBuilder leftJoin(
+    String table,
+    String firstColumn, [
+    String? operator,
+    String? secondColumn,
+    bool where = false,
+  ]) {
+    return join(
+      table,
+      firstColumn,
+      operator,
+      secondColumn,
+      "left",
+      where,
+    );
+  }
+
+  @override
+  QueryBuilder leftJoinSub(
+    QueryBuilder subQuery,
+    String as,
+    String firstColumn, [
+    String? operator,
+    String? secondColumn,
+  ]) {
+    return joinSub(
+      subQuery,
+      as,
+      firstColumn,
+      operator,
+      secondColumn,
+      "left",
+    );
+  }
+
+  @override
+  QueryBuilder rightJoin(
+    String table,
+    String firstColumn, [
+    String? operator,
+    String? secondColumn,
+  ]) {
+    String clause = "RIGHT JOIN $table";
+    if (operator != null && secondColumn != null) {
+      clause += " ON $firstColumn $operator $secondColumn";
+    }
+    joins.add(clause);
+    return this;
+  }
+}
