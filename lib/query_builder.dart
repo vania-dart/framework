7c468d48b8b3901336c0a01d37d5ac2d9957f3a1
Javad Zobeidi <45537050+javad-zobeidi@users.noreply.github.com>
2025-05-16 23:53:45 +0330

    centralized error handling and add `extract Params` utility

--- lib/query_builder.dart
+++ lib/query_builder.dart
@@ -1,3 +1,5 @@
 export 'src/database/db.dart';
 export 'src/contract/database/query_builder/query_builder.dart';
 export 'src/database/monitoring/database_monitor.dart';
+export 'src/exception/database_exception.dart';
+export 'src/exception/query_exception.dart';
