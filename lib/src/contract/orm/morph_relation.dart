9cea757b7e9abaecc16b6be39c862b4486684fd5
Javad Zobeidi <45537050+javad-zobeidi@users.noreply.github.com>
2025-07-22 00:19:38 +0330

    refactor(orm): fix eager-loading for polymorphic relations (MorphTo, MorphToMany, MorphedByMany)

--- lib/src/contract/orm/morph_relation.dart
+++ lib/src/contract/orm/morph_relation.dart
@@ -4,11 +4,15 @@ abstract class MorphRelation extends Relation {
   final String morphKey;
   final String morphType;
   final String? type;
+  final String? pivotTable;
+  final String? relatedMorphKey;
   MorphRelation({
     required super.related,
     required super.parent,
     required this.morphKey,
     required this.morphType,
+    this.pivotTable,
+    this.relatedMorphKey,
     super.localKey = 'id',
     this.type,
   });
--- lib/src/database/orm/polymorphic/morph_to_many.dart
+++ lib/src/database/orm/polymorphic/morph_to_many.dart
@@ -6,6 +6,8 @@ class MorphToMany extends MorphRelation {
     required super.related,
     required super.morphKey,
     required super.morphType,
+    required super.pivotTable,
+    required super.relatedMorphKey,
     super.type,
     super.localKey = 'id',
   });
