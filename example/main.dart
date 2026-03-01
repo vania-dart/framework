afb79f45f6e80d4f9cea7674b65ee900d7407c8e
apple <apple@appledeMacBook-Air.local>
2026-03-01 13:07:41 +0800

    refactor: move app_exception_handler to example directory

--- example/main.dart
+++ example/main.dart
@@ -1,5 +1,5 @@
 import 'package:vania/vania.dart';
-import 'package:vania/src/exception/app_exception_handler.dart';
+import 'app_exception_handler.dart';
 
 void main() async {
   Application().initialize(
